852d96ec382c89391fbdbce9548509aa4fea0e16
yaopin002 <379295901@qq.com>
2017-12-11 15:29:34 +0800

    second commit

---
+++ MSCDemo/Assets/XunFeiTest.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class XunFeiTest : MonoBehaviour
+{
+	private string showResult = "";
+
+	void OnGUI ()
+	{
+		if (GUILayout.Button ("startRecognizer", GUILayout.Height (100))) {  
+			AndroidJavaClass jc = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");  
+			AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject> ("currentActivity");
+            jo.Call("StartSpeech");  
+		}  
+
+		GUILayout.TextArea (showResult, GUILayout.Width (200));  
+
+	}
+
+    public void SpeechResult(string recognizerResult)
+	{  
+		showResult += recognizerResult;  
+	}
+}  
